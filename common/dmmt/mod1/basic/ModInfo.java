b8c83245d705ed85e39738211e9cb3628943d490
ianno1993 <i.lol@hotmail.com>
2014-02-19 17:57:39 +0800

    Version changed to Alpha stage
    Like the title says, Version changed to Alpha stage.

--- common/dmmt/mod1/basic/ModInfo.java
+++ common/dmmt/mod1/basic/ModInfo.java
@@ -4,7 +4,7 @@ public class ModInfo {
 
 	public static final String ID = "Basic";
 	public static final String NAME = "Basic";
-	public static final String VERSION = "1.0.0";
+	public static final String VERSION = "1.0.0A";
 	public static final String CLIENTPROXY = "dmmt.mod1.basic.client.";
 	public static final String COMMONPROXY = "dmmt.mod1.basic.";
 	
