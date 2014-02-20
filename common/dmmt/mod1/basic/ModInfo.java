84f62f0579edd3fabf3b6dd6fdfcfd0d6341ed05
wargamer88 <m.van.denzel@home.nl>
2014-02-20 13:08:26 +0100

    1.6.4 - 0.0.12A: added 2 block textures
    added andesite and siltstone textures.

--- common/dmmt/mod1/basic/ModInfo.java
+++ common/dmmt/mod1/basic/ModInfo.java
@@ -4,7 +4,7 @@ public class ModInfo {
 
 	public static final String ID = "MC-MOD-01";
 	public static final String NAME = "MC-MOD-01";
-	public static final String VERSION = "1.6.4 - 0.0.10A";
+	public static final String VERSION = "1.6.4 - 0.0.12A";
 	public static final String CLIENTPROXY = "dmmt.mod1.basic.client.";
 	public static final String COMMONPROXY = "dmmt.mod1.basic.";
 	
